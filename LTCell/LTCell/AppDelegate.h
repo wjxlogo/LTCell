83dd9f8123327f32b432b069ee916b65c1a44264
why <why@163.com>
2016-06-03 10:47:23 +0800

    2016年06月03日10:47:16  初始化项目

---
+++ LTCell/LTCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LTCell
+//
+//  Created by WJX on 16/6/3.
+//  Copyright © 2016年 wjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LTCell/LTCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LTCell
+//
+//  Created by WJX on 16/6/3.
+//  Copyright © 2016年 wjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
